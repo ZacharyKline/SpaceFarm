29b905ca44967d0c1a6d259990680dbb3353a9a8
Zachary Kline <zkline888@gmail.com>
2020-05-27 10:10:38 -0400

    Did a few UI updates, and added the ability to trade sheep for parts, in its most basic form

--- scripts/resources.js
+++ scripts/resources.js
@@ -13,7 +13,6 @@ let eggCount = 0  //Eggs per second
 let gmilkCount = 0  //Goats Milk per second?
 let chaosCount = 0
 let dEggCount = 0
-let squaredEyes = 0  //Special rare sheep currency
 let timeValue = 1500
 let incrementTime = (Math.random()+.1)*10
 let sheepUpgrade1 = 1
