3c2f04a493309c1d079a478d5fe7df8fc1dce031
Zachary Kline <zkline888@gmail.com>
2020-06-17 11:11:07 -0400

    added some basic stuff for the unicorn

--- scripts/lab.js
+++ scripts/lab.js
@@ -1,1 +1,16 @@
+//Imports for the various abominations you can make
+const unicornImage = document.querySelector('#unicornImage');
+const unicornButt = document.querySelector('#unicornButt');
 
+
+
+//Unlock intervals
+
+const unicornInterval = setInterval(function () {
+    //Add in unicorn related unlocks here
+	if (bloodCount > 400) {
+		unicornImage.innerHTML ='<img src="./img/unicorn.jpeg" class="animals" height=100 width=100/>';
+		unicornButt.innerHTML ='<button onclick="buyUnicorn()">Make a Unicorn</button>';
+		stop(unicornInterval);
+	}
+}, 5000);
